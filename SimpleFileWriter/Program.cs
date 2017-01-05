633686203793e0e339ecf976514d7567b43e8979
brueckmann <chrisnain@hotmail.com>
2017-01-05 16:55:34 +0100

    Added SimpleFileWriter.

---
+++ SimpleFileWriter/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleFileWriter
+{
+	using System.IO;
+
+	class Program
+	{
+		static void Main()
+		{
+			using (var streamWriter = new StreamWriter("D:\\Temp\\sasUrl.txt"))
+			{
+				streamWriter.Write("http://stackoverflow.com/users/909980/christoph-br%c3%bcckmann");
+			}
+		}
+	}
+}
